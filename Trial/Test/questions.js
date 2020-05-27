16af0ec4a31a1daa7c8ab4e280eb7bf7c7340772
Enron Hubbard <tygerhardt@gmail.com>
2020-05-26 20:07:35 -0700

    Updating .css .html .js files.

---
+++ Trial/Test/questions.js
@@ -0,0 +1,17 @@
+var questions = [{
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "The question is the last one in the array ____.",
+    choices: ["uno", "dos", "tres", "quatro"],
+    answer: "tres"
+  },
+];
+
